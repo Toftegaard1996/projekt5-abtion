e358f8fc6fcaf6ce0d7362c511343b7539cf9ff0
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-04-15 09:50:12 +0200

    Javascript og små rettelser

--- javascript/lille_sky.js
+++ javascript/lille_sky.js
@@ -2,6 +2,10 @@ function parentTilbage() {
     window.location.href = 'index.html';
 }
 
+function parentSpil() {
+    window.location.href = 'spil.html';
+}
+
 function spilTilbage() {
     window.location.href = 'index.html';
 }
