a24919e950490b1e7b8c81f3698d9da40445054b
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-04-15 16:53:46 +0200

    Få ændringer og js

--- javascript/lille_sky.js
+++ javascript/lille_sky.js
@@ -1,3 +1,5 @@
+/* Knapper */
+
 function tilbage() {
     window.location.href = 'index.html';
 }
@@ -6,3 +8,9 @@ function spil() {
     window.location.href = 'spil.html';
 }
 
+function dreng() {
+    document.getElementsByClassName("boySelected").style.display = "block";
+<div class="boySelected">
+               <img src="billeder/Boy.png" alt="Dreng" class="boy2">
+           </div>
+}
