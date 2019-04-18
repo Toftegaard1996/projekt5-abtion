5a9431ff940c8c3badb007b8376b112032363706
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-04-18 12:40:42 +0200

    Mere js
    Mere javascript på forsiden

--- javascript/lille_sky.js
+++ javascript/lille_sky.js
@@ -8,13 +8,26 @@ function spil() {
     window.location.href = 'spil.html';
 }
 
+function vejrSpil() {
+    let getVejrKnap = document.querySelector(".vejr").style.display = "hidden";
+    let getKortSpil = document.querySelector(".spilValg").style.display = "visible";
+}
+
 function dreng() {
-    document.getElementsByClassName("boySelected").style.display = "block";
+    document.querySelector("boySelected").style.display = "block";
 
 }
+
+/* timing */
 
 function lilleSky() {
     let getLilleSky = document.querySelector(".lille-sky").style.visibility = "hidden";
 }
 
+function vejr() {
+    let getVejr = document.querySelector(".vejr").style.visibility = "visible";
+}
+
 setTimeout(lilleSky, 3000);
+
+setTimeout(vejr, 3000);
