169fdd10f291f3ad8582c92eec121842f0ccc128
Lepengui <47316239+Lepengui@users.noreply.github.com>
2019-04-22 10:35:01 +0200

    Burgermenu

--- javascript/lille_sky.js
+++ javascript/lille_sky.js
@@ -26,3 +26,25 @@ function vejr() {
 setTimeout(lilleSky, 3000);
 
 setTimeout(vejr, 3000);
+
+/* Burgermenu (Jeg har fulgt en tutoriel fra mmtuts på youtube, men rettet koden til hvor jeg mente det er nødvendigt)*/
+
+let toggleMenuStatus = false;
+
+let toggleMenu = function() {
+    let getMenuBoks = document.querySelector(".menu-boks");
+    let getMenuBoksUl = document.querySelector(".menu-boks ul");
+    let getMenuBoksTitle = document.querySelector(".menu-boks span");
+    let getMenuBoksLinks = document.querySelectorAll(".menu-boks a");
+    
+    if (toggleMenuStatus === false) {
+        getMenuBoksUl.style.visibility = "visible";
+        getMenuBoks.style.width = "272px";
+        getMenuBoksTitle.style.opacity = "0.5";
+        
+        let arrayLength = getMenuBoksLinks.length;
+        for (let i = 0; i < arrayLength; i++) {
+            
+        }
+    }
+}
