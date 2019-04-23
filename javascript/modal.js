9d889b8497068d4cc3d9911c75ee7aed1489dc29
aytepala <32264500+aytepala@users.noreply.github.com>
2019-04-23 14:20:19 +0200

    Ekstra ven tilføjet
    Kanin

--- javascript/modal.js
+++ javascript/modal.js
@@ -18,3 +18,24 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+// henter modal
+var modal = document.getElementById('myModal2');
+
+// sætter billedet inde i modal- alt på billede bliver brugt som tekst
+var img = document.getElementById('myImg2');
+var modalImg = document.getElementById("bigVen2");
+var captionText = document.getElementById("caption2");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// span elementet bliver hentet til at lukke
+var span = document.getElementsByClassName("close2")[0];
+
+// når man klipper på x, lukker modal ned
+span.onclick = function() { 
+  modal.style.display = "none";
+}
