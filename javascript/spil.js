0943e377a4a1a1961c36f0f1c8fe075a1f1a399a
JulieBerntsen <julieberntsen98@hotmail.com>
2019-04-24 08:25:01 +0200

    Spil

--- javascript/spil.js
+++ javascript/spil.js
@@ -4,7 +4,9 @@ function tilbage() {
 
 let startSpilStatus = true;
 
+
 let startSpil = function() {
+
     let getSpilStartet = document.querySelector(".choice1");
     let getMap = document.querySelector(".map");
     
@@ -20,9 +22,17 @@ let rigtigtValgParaply = function() {
     let getRigtigtValgParaply = document.querySelector(".choice2");
     let getChoice1 = document.querySelector(".choice1");
     
+    let getSpilStartet = document.querySelector(".choice1");
+    let getMap2 = document.querySelector(".mapLevel2");
+    
     if (rigtigtValgParaplyStatus === true){
         getRigtigtValgParaply.style.visibility = "visible";
         getChoice1.style.visibility = "hidden"; 
+        setTimeout(function(){ 
+            getRigtigtValgParaply.style.visibility = "hidden";
+            getSpilStartet.style.visibility = "hidden";
+            getMap2.style.visibility = "visible";
+        },2000);
     }
 }
 
@@ -32,15 +42,22 @@ let forkertValgSolbriller = function() {
     let getForkertValgSolbriller = document.querySelector(".choice3");
     let getChoice1 = document.querySelector(".choice1");
     
+    let getSpilStartet = document.querySelector(".choice1");
+    let getMap = document.querySelector(".map");
+    
     if (forkertValgSolbrillerStatus === true){
         getForkertValgSolbriller.style.visibility = "visible";
         getChoice1.style.visibility = "hidden"; 
+                setTimeout(function(){ 
+            getForkertValgSolbriller.style.visibility = "hidden";
+        getChoice1.style.visibility = "visible"; 
+                    
+            
+        },2000);
     }
 }
 
-
 /* LEVEL 2: TEST */
-
 
 
 
